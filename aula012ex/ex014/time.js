fe7e3b6976373088f65b4a0ac07b83fb2c8d8734
Tiago C Fortunato <120049822+TiagoCarvalhoFortunato@users.noreply.github.com>
2022-12-29 09:29:13 -0300

    e os estudos continua

--- aula012ex/ex014/time.js
+++ aula012ex/ex014/time.js
@@ -1,4 +1,18 @@
-var horas = new Date()
-var hora = horas.getHours()
-var minuto = horas.getMinutes()
- 
+function carregar() {
+    var msg = window.document.getElementById('msg')
+    var img = window.document.getElementById('imagem')
+    var data = new Date()
+    var hora = data.getHours()
+    msg.innerHTML = 'Agora são ' + hora + ' horas.'
+    if (hora > 4 && hora < 12) {
+        img.src = 'imagens/manha.png'
+        document.body.style.backgroundImage = 'linear-gradient(to right, orange, white)'
+    } else if (hora >= 12 && hora < 18) {
+        img.src = 'imagens/tarde.png'
+        document.body.style.backgroundImage = 'linear-gradient(to right, orange, yellow)'
+    } else {
+        img.src = 'imagens/noite.png'
+        document.body.style.backgroundImage = 'linear-gradient(to right, grey 80%, white)'
+    }
+
+}
