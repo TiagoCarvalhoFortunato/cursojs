98593e1d00150cde94d573e6574149f57a167334
Tiago C Fortunato <120049822+TiagoCarvalhoFortunato@users.noreply.github.com>
2022-12-29 08:12:13 -0300

    estudando

--- aula012/ex012.js
+++ aula012/ex012.js
@@ -4,7 +4,7 @@
  console.log('Agora são exatamente ' + hora + ':' + minuto)
  if (hora > '18') {
     console.log('Boa Noite!')
- } else if (hora <= '18' || hora >= '12') {
+ } else if (hora <= '18' && hora >= '12') {
     console.log('Boa Tarde!')
  } else {
     console.log('Bom Dia!')
