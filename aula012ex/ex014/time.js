775f0bfe5e23084e3d41837542b34f64975f5dac
Tiago C Fortunato <120049822+TiagoCarvalhoFortunato@users.noreply.github.com>
2022-12-29 09:41:45 -0300

    Update time.js

--- aula012ex/ex014/time.js
+++ aula012ex/ex014/time.js
@@ -3,16 +3,20 @@ function carregar() {
     var img = window.document.getElementById('imagem')
     var data = new Date()
     var hora = data.getHours()
-    msg.innerHTML = 'Agora são ' + hora + ' horas.'
+    msg.innerHTML = '<p>Agora são ' + hora + ' horas.</p>'
     if (hora > 4 && hora < 12) {
         img.src = 'imagens/manha.png'
         document.body.style.backgroundImage = 'linear-gradient(to right, orange, white)'
+        msg.innerHTML += 'Bom dia !'
+
     } else if (hora >= 12 && hora < 18) {
         img.src = 'imagens/tarde.png'
         document.body.style.backgroundImage = 'linear-gradient(to right, orange, yellow)'
+        msg.innerHTML += 'Boa Tarde !'
     } else {
         img.src = 'imagens/noite.png'
         document.body.style.backgroundImage = 'linear-gradient(to right, grey 80%, white)'
+        msg.innerHTML += 'Boa noite !'
     }
 
 }
