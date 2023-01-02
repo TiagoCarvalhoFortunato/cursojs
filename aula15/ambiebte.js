44a0c60fa85f7e60de225d66a00e13a703f1d439
Tiago C Fortunato <120049822+TiagoCarvalhoFortunato@users.noreply.github.com>
2023-01-02 16:24:55 -0300

    estudando

---
+++ aula15/ambiebte.js
@@ -0,0 +1,11 @@
+var valores = [2, 8, 6, 4]
+
+/*for (let seq = 0; seq < valores.length; seq++){
+    valores.sort()
+    console.log(' Os valores são ' + valores[seq])
+}*/
+
+for(seq in valores) {
+    valores.sort()
+    console.log(' Os valores são este ai ' + valores[seq])
+}
